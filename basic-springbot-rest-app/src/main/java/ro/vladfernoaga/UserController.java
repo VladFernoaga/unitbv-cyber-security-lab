878248066363397cded7a42055c1bf3b6e1c01f9
vlad <fernoaga.vlad@gmail.com>
2019-04-14 22:57:22 +0300

    add basic security

--- basic-springbot-rest-app/src/main/java/ro/vladfernoaga/UserController.java
+++ basic-springbot-rest-app/src/main/java/ro/vladfernoaga/UserController.java
@@ -3,6 +3,8 @@ package ro.vladfernoaga;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,11 +16,24 @@ public class UserController {
 
 	private final AtomicLong counter = new AtomicLong();
 
+	@CrossOrigin
+	@GetMapping("/public")
+	public String helloPublic() {
+		return "Hello Public!";
+	}
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/private")
+	public String helloPrivate() {
+		return "Hello Private!";
+	}
+
+
 	@GetMapping("/user")
 	public User getUser(@RequestParam(value = "name") String name) {
 		return new User(counter.incrementAndGet(), name, new Random().nextInt(99));
 	}
-	
+
 	@PostMapping("/user")
 	public String addUser(@RequestBody User greeting) {
 		return "User added sucessfuly";
