1dbc49649bc1d0365c1ccf606f9978b472d2fd08
Vlad Fernoaga <vlad.fernoaga@cerner.com>
2021-03-24 19:14:54 +0200

    customizations of Basic auth

--- spring-boot-basic-authentication/src/main/java/pl/rmitula/springbootbasicauthentication/Controller/HelloController.java
+++ spring-boot-basic-authentication/src/main/java/pl/rmitula/springbootbasicauthentication/Controller/HelloController.java
@@ -20,7 +20,7 @@ public class HelloController {
         return "Hello Private!";
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/admin")
     public String HelloAdmin() {
 
--- spring-boot-basic-authentication/src/main/java/pl/rmitula/springbootbasicauthentication/Security/SecurityConfig.java
+++ spring-boot-basic-authentication/src/main/java/pl/rmitula/springbootbasicauthentication/Security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -19,9 +20,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//    	http.authorizeRequests().anyRequest().authenticated();
+    	http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
                 .authorizeRequests()
                 .antMatchers("/public/*").permitAll()
+                .antMatchers("/admin/").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
